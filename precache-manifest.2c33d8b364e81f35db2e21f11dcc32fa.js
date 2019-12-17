1f937971b82ed7309f318bca59c337f47d51db76
abl3l <7ckngmad.mn@gmail.com>
2019-12-17 20:35:24 +0200

    Updates

---
+++ precache-manifest.2c33d8b364e81f35db2e21f11dcc32fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4bfbb7e923d6ead4a6050aaa92a703",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "3a3127ecfa4bb7f140a6",
+    "url": "/goit-react-hw-01-components/static/js/2.3836b913.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/goit-react-hw-01-components/static/js/2.3836b913.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69a1fbd71bfbd079c820",
+    "url": "/goit-react-hw-01-components/static/js/main.53752201.chunk.js"
+  },
+  {
+    "revision": "351ec24c9bd9afb33e31",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.b5c70b18.js"
+  }
+]);
