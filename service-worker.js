d108526898b13e615de44a66d4763ed8edb42736
abl3l <7ckngmad.mn@gmail.com>
2019-12-22 04:17:14 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.5be2227a4463c5201cf6bfbc1ee59d7a.js"
+  "/goit-react-hw-01-components/precache-manifest.03edac208f1b234f564e18a0f4b62103.js"
 );
 
 self.addEventListener('message', (event) => {
